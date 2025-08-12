7250793c416f5dfb8846f56255b11ef739789ba5
SCORP-XD <jopaok228@gmail.com>
2025-08-12 16:54:41 +0000

    Update mega.js

--- mega.js
+++ mega.js
@@ -1,8 +1,8 @@
 const mega = require("megajs");
 
 const auth = {
-    email: 'permaunban@gmail.com',   //use your real vaild mega account email
-    password: 'umar165123719',  ////use your real vaild mega account password
+    email: 'scorpionv1az@gmail.com',   //use your real vaild mega account email
+    password: 'MAZALO228@@##',  ////use your real vaild mega account password
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 };
 
