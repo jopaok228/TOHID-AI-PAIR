6de08071778494f605958a77c239bf18cc444641
SCORP-XD <jopaok228@gmail.com>
2025-08-12 17:00:06 +0000

    Update qr.js

--- qr.js
+++ qr.js
@@ -16,7 +16,7 @@ const MESSAGE = process.env.MESSAGE ||  `
 *SESSION GENERATED SUCCESSFULY* ✅
 
 *Gɪᴠᴇ ᴀ ꜱᴛᴀʀ ᴛᴏ ʀᴇᴘᴏ ꜰᴏʀ ᴄᴏᴜʀᴀɢᴇ* 🌟
-https://github.com/Tohidkhan6332/TOHID-AI
+https://github.com/jopaok228/SCORP_XD
 
 *Tᴇʟᴇɢʀᴀᴍ Gʀᴏᴜᴘ* 🌟
 https://t.me/Tohid_Tech
@@ -31,7 +31,7 @@ https://whatsapp.com/channel/0029VaGyP933bbVC7G0x0i2T
 https://youtube.com/Tohidkhan_6332
 
 *ɢɪᴛʜᴜʙ* 🌟
-http://GitHub.com/Tohidkhan6332
+https://github.com/jopaok228/SCORP_XD
 
 *Wᴇʙsɪᴛᴇ* 🌟
 https://tohid-khan-web.vercel.app/
